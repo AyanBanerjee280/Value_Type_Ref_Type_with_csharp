448797ad93814dc6b85aa1a9667656b36623fee4
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-13 13:12:09 +0530

    Add files via upload

---
+++ Value_Type_Ref_Type.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Value_Ref_Type
+{//Objects of Value type are strored in Stack Memory
+//Objects of reference type are stored in Heap memory
+    struct Employee
+    {//Struct is a value type
+        public int salary;
+        public int age;
+    }
+    class Reference
+    {//class is a reference type
+        static void Main(string[] args)
+        {
+            Employee obj = new Employee();//These objects will be stored in stack memory(value type)
+            obj.salary = 150000;
+            obj.age = 35;
+            Employee obj1 = obj;
+            Employee obj2 = obj;
+            obj.age = 38;
+
+            Console.WriteLine("Revised age: " + obj.age);//Revised age
+            Console.WriteLine("Previous age: " + obj1.age);//Age is not updated
+            Console.WriteLine("Previous age: " + obj2.age);//Age is not updated
+            Console.ReadLine();
+        }
+    }
+}
