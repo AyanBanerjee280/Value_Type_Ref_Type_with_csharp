1f78ff34aef1424665391f9abc9491adfd175803
Ayan Banerjee <108017943+AyanBanerjee280@users.noreply.github.com>
2022-07-13 13:20:24 +0530

    Add files via upload

---
+++ Pass_By_Value.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Value_Ref_Type
+{//pass by value
+    class Program
+    {//Class is a reference type
+        public static void PassByValue(int a)
+        {
+            a = a + 10;
+            Console.WriteLine("Value is {0} ", a);
+        }
+        public static void Main(string[] args)
+        {
+            int value = 5;
+            Program.PassByValue(10);//Result will be 20
+            PassByValue(value);//Results will be 15
+            Console.WriteLine(value);
+            Console.Read();
+        }
+    }
+}
